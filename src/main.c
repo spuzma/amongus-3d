4f76e6c0fdbcf15dd112b42277ffcfad9a44a2c0
cjrt <ctalb5000@gmail.com>
2024-02-25 17:38:18 +0000

    omgg squaree

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 384
-#define SCREEN_HEIGHT 216
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 // Implement render loop
 static void render();
@@ -23,11 +24,19 @@ int main(int argc, char *args[]) {
     return -1;
   }
   state.window =
-      SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      SDL_CreateWindow("Monke and Dunke", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        1280, 720, SDL_WINDOW_ALLOW_HIGHDPI);
 
   state.renderer =
       SDL_CreateRenderer(state.window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  SDL_SetRenderDrawColor(state.renderer, 255, 0, 0, 255);
+
+  SDL_Rect player;
+  player.x = 0;
+  player.y = 0;
+  player.w = 100;
+  player.h = 100;
 
   while (!state.quit) {
     // main game loop
@@ -36,11 +45,27 @@ int main(int argc, char *args[]) {
       // User requests quit
       if (e.type == SDL_QUIT) {
         state.quit = true;
+      } else if (e.type == SDL_KEYDOWN) {
+          switch (e.key.keysym.sym) {
+          case SDLK_w:
+            player.y -= 10;
+            break;
+          case SDLK_s:
+            player.y += 10;
+            break;
+          case SDLK_a:
+            player.x -= 10;
+            break;
+          case SDLK_d:
+            player.x += 10;
+            break;
+        }
       }
     }
     SDL_UpdateTexture(state.texture, NULL, state.pixels, SCREEN_WIDTH * 4);
     SDL_RenderCopyEx(state.renderer, state.texture, NULL, NULL, 0.0, NULL,
                      SDL_FLIP_VERTICAL);
+    SDL_RenderFillRect(state.renderer, &player);
     SDL_RenderPresent(state.renderer);
   }
 
