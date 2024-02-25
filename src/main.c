814d089d1a0e07fe324e7e499c2e4ee47a19a71b
spuzma <128107239+spuzma@users.noreply.github.com>
2024-02-25 01:36:10 +0100

    yes

--- src/main.c
+++ src/main.c
@@ -1,27 +1,39 @@
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_timer.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-struct Screen {
+struct {
   SDL_Window *window;
-  SDL_Surface *window_surface;
-};
+  SDL_Renderer *renderer;
+  SDL_Texture *texture;
+  bool quit;
+} state;
 
 int main(int argc, char *args[]) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Failed to initialize the SDL2 library\n");
     return -1;
   }
-  struct Screen screen;
-  screen.window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED,
-                                   SDL_WINDOWPOS_CENTERED, 700, 400, 0
+  state.window =
+      SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       1280, 720, SDL_WINDOW_ALLOW_HIGHDPI);
 
-  );
-  screen.window_surface = SDL_GetWindowSurface(screen.window);
+  state.renderer =
+      SDL_CreateRenderer(state.window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-  SDL_FillRect(screen.window_surface, NULL,
-               SDL_MapRGB(screen.window_surface->format, 255, 90, 120));
-  SDL_UpdateWindowSurface(screen.window);
-  SDL_Delay(10000);
+  while (!state.quit) {
+    // main game loop
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0) {
+      // User requests quit
+      if (e.type == SDL_QUIT) {
+        state.quit = true;
+      }
+    }
+  }
+
+  SDL_DestroyTexture(state.texture);
+  SDL_DestroyRenderer(state.renderer);
+  SDL_DestroyWindow(state.window);
 }
