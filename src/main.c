0b6bb98e40dc7c8bc8df227087d9b1a1102429c4
spuzma <128107239+spuzma@users.noreply.github.com>
2024-02-25 22:57:05 +0100

    fixed up the rect suff some

--- src/main.c
+++ src/main.c
@@ -1,42 +1,47 @@
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_rect.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-// Implement render loop
-static void render();
-
 struct {
   SDL_Window *window;
   SDL_Renderer *renderer;
   SDL_Texture *texture;
-  uint32_t pixels[SCREEN_HEIGHT * SCREEN_WIDTH];
   bool quit;
+
+  struct {
+    SDL_Rect body;
+
+  } player;
+
 } state;
+
+// Implement render loop
+static void render() {
+  SDL_SetRenderDrawColor(state.renderer, 255, 0, 0, 255);
+  SDL_RenderFillRect(state.renderer, &state.player.body);
+};
 
 int main(int argc, char *args[]) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Failed to initialize the SDL2 library\n");
     return -1;
   }
-  state.window =
-      SDL_CreateWindow("Monke and Dunke", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       1280, 720, SDL_WINDOW_ALLOW_HIGHDPI);
+  state.window = SDL_CreateWindow("Monke and Dunke", SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                                  SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
   state.renderer =
       SDL_CreateRenderer(state.window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-  SDL_SetRenderDrawColor(state.renderer, 255, 0, 0, 255);
-
-  SDL_Rect player;
-  player.x = 0;
-  player.y = 0;
-  player.w = 100;
-  player.h = 100;
+  state.player.body.x = SCREEN_WIDTH / 2;
+  state.player.body.y = SCREEN_HEIGHT / 2;
+  state.player.body.w = 100;
+  state.player.body.h = 100;
 
   while (!state.quit) {
     // main game loop
@@ -46,26 +51,27 @@ int main(int argc, char *args[]) {
       if (e.type == SDL_QUIT) {
         state.quit = true;
       } else if (e.type == SDL_KEYDOWN) {
-          switch (e.key.keysym.sym) {
-          case SDLK_w:
-            player.y -= 10;
-            break;
-          case SDLK_s:
-            player.y += 10;
-            break;
-          case SDLK_a:
-            player.x -= 10;
-            break;
-          case SDLK_d:
-            player.x += 10;
-            break;
+        switch (e.key.keysym.sym) {
+        case SDLK_w:
+          state.player.body.y -= 10;
+          break;
+        case SDLK_s:
+          state.player.body.y += 10;
+          break;
+        case SDLK_a:
+          state.player.body.x -= 10;
+          break;
+        case SDLK_d:
+          state.player.body.x += 10;
+          break;
         }
       }
     }
-    SDL_UpdateTexture(state.texture, NULL, state.pixels, SCREEN_WIDTH * 4);
-    SDL_RenderCopyEx(state.renderer, state.texture, NULL, NULL, 0.0, NULL,
-                     SDL_FLIP_VERTICAL);
-    SDL_RenderFillRect(state.renderer, &player);
+
+    SDL_SetRenderDrawColor(state.renderer, 0, 0, 0, 255);
+    SDL_RenderClear(state.renderer);
+
+    render();
     SDL_RenderPresent(state.renderer);
   }
 
